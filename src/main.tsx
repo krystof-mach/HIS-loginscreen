f558d0af64c71eb5b1322554bd47fdbec1545dfe
Krystof Mach <krystofmach@skolakrizik.cz>
2025-05-28 16:32:43 +0200

    +responsive design
    +tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import App from './App';
-import Visuals from './Visuals.tsx'
 import './index.css';
 import '@mantine/core/styles.css';
+import 'tailwindcss'
+import { createTheme, TextInput } from '@mantine/core';
 
+
+const theme = createTheme({
+    components: {
+        TextInput: TextInput.extend({
+            vars: (theme,props) =>{
+                return {
+                    input: {"--input-bg": "rgba(255, 255, 255, 0.4)"},
+                    : {"--input-placeholder-color": "rgb(0, 0,0)"}
+                };
+            }
+        }),
+    }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
